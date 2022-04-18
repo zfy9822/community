8a108e74a7beef94af6950167471e7e193df9447
zfy <517529590@qq.com>
2022-04-18 13:38:54 +0800

    开发登录、退出功能

---
+++ src/test/java/com/nowcoder/community/MailTests.java
@@ -0,0 +1,43 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+/**
+ * @Author: zfy
+ * @Date: 2022/4/12 10:36
+ * @Description:
+ */
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail(){
+        mailClient.sendMail("517529590@qq.com", "TEST", "hello");
+    }
+
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("517529590@qq.com", "HTML", content);
+
+
+    }
+}
